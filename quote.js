b98cdeaeff1cd110407b63d792f253a35f7a8b27
shaoli-lu <shaoli_lu@yahoo.com>
2021-04-16 21:50:35 -0500

    Initial checkin

---
+++ quote.js
@@ -0,0 +1,44 @@
+let pic = document.getElementById('pic');
+
+document.addEventListener('DOMContentLoaded', function() {
+getQuote();
+
+})
+
+function getQuote() {
+    fetch("https://icanhazdadjoke.com/slack").then(response => response.json()).then(data => {
+    
+   
+        document.querySelector('#quote').innerHTML = '"' + data.attachments[0].fallback  + '"' ;  
+       
+        
+            
+    })  
+}
+
+pic.addEventListener('click', getQuote)
+
+$(document).ready(function () {
+
+    $('xdiv').hover(
+    function () {
+        $(this).stop().fadeOut(function () {
+            var $temp = $(this).attr('src');
+            $(this).attr('src', $(this).attr('data-alt-src'));
+            $(this).attr('data-alt-src', $temp);
+        });
+
+        $(this).fadeIn();
+    },
+
+    function () {
+        $(this).stop().fadeOut(function () {
+            var $temp = $(this).attr('data-alt-src');
+            $(this).attr('data-alt-src', $(this).attr('src'));
+            $(this).attr('src', $temp);
+        });
+
+        $(this).fadeIn();
+
+    });
+});
